41b8cf80d3ff5871fa480e62c88321dcab6fbd06
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2025-11-13 14:51:33 -0500

    nation component styling and name and color fields

--- src/NationSheet.tsx
+++ src/NationSheet.tsx
@@ -4,6 +4,14 @@ function NationSheet() {
       <section className="flag-selector">
         <p>Flag</p>
       </section>
+      <section className="nation-name">
+        <p>Name</p>
+        <input type="text"></input>
+      </section>
+      <section className="nation-color">
+        <p>Color</p>
+        <input type="color"></input>
+      </section>
     </section>
   );
 }
