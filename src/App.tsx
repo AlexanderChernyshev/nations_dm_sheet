169f3b6282e19b224be46c7427400aa9501f4200
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2026-01-23 18:52:27 -0500

    nation sheet add button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,29 @@ import DiceRoller from "./DiceRoller";
 import DateTracker from "./DateTracker";
 import NationSheet from "./NationSheet";
 import NationSheetAddButton from "./NationSheetAddButton";
+import { useState } from "react";
 
 function App() {
+  function getRandID() {
+    const min: number = 1;
+    const max: number = 999;
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  const [nationsSheetsList, setNationsSheetsList] = useState([
+    getRandID(),
+    getRandID(),
+    getRandID(),
+  ]);
+
+  const nationSheets = nationsSheetsList.map((id) => <NationSheet key={id} />);
+
+  function addSheet() {
+    setNationsSheetsList([...nationsSheetsList, getRandID()]);
+  }
+
   return (
     <>
       <h1>Nations DM/GM Sheet</h1>
@@ -15,10 +36,8 @@ function App() {
 
       <section className="nations-sheet">
         <h2>Nations</h2>
-        <NationSheet />
-        <NationSheet />
-        <NationSheet />
-        <NationSheetAddButton />
+        {nationSheets}
+        <button onClick={addSheet}>+</button>
       </section>
     </>
   );
